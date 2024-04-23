ece1c4d6293b8f5be59d3740b721c67d907c040d
TobyYang7 <tobyyang7@foxmail.com>
2024-04-23 17:24:02 +0800

    new data

--- train_LLM/prepare_flight_review_dataset.py
+++ train_LLM/prepare_flight_review_dataset.py
@@ -17,15 +17,15 @@ def format_review(df):
     return df
 
 
-csv_file_path = '/home/zhangmin/toby/IBA_Project_24spr/data/flight_review/Tweets.csv'
+csv_file_path = '../data/flight_review/Tweets.csv'
 df = pd.read_csv(csv_file_path, quotechar='"', escapechar='\\', engine='python', on_bad_lines='skip')
 train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
 train_df = format_review(train_df)[:3000]
 test_df = format_review(test_df)
 
-train_json_path = '/home/zhangmin/toby/IBA_Project_24spr/data/flight_review/train.json'
-test_json_path = '/home/zhangmin/toby/IBA_Project_24spr/data/flight_review/test.json'
+train_json_path = '../data/flight_review/train.json'
+test_json_path = '../data/flight_review/test.json'
 train_df.to_json(train_json_path, orient='records', force_ascii=False, lines=False, indent=2)
 test_df.to_json(test_json_path, orient='records', force_ascii=False, lines=False, indent=2)
 

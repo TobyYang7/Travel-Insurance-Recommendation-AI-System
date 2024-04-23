23813c3726c803b78b741f6e398a73637849efc2
TobyYang7 <tobyyang7@foxmail.com>
2024-04-23 14:44:14 +0800

    saves

--- train_LLM/InsuranceGPT_exp.py
+++ train_LLM/InsuranceGPT_exp.py
@@ -10,38 +10,18 @@ os.environ['HF_HUB_ENABLE_HF_TRANSFER'] = '1'
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 exp1_config = {
-    "model_name_or_path": "/home/zhangmin/.kaggle/chatglm2-6b/",
-    "template": "chatglm2",
+    "model_name_or_path": "Qwen/Qwen1.5-7B-Chat",
+    "template": "qwen",
     "dataset_dir": "/home/zhangmin/toby/IBA_Project_24spr/data",
-    "dataset": "InsuranceCorpus",
+    "dataset": "sentiment_test",
     # "cutoff_len": 1024,
     "per_device_train_batch_size": 2,
     "max_new_tokens": 200,
     "top_p": 0.8,
     "temperature": 0.8,
-    "output_dir": "/home/zhangmin/toby/IBA_Project_24spr/saves/exp/glm_6b",
+    "output_dir": "/home/zhangmin/toby/IBA_Project_24spr/saves/exp/insurance_qwen_7b_sentiment",
     "do_predict": True,
     "predict_with_generate": True,
-    "split": "validation",
-    "max_samples": 100,
-}
-exp2_config = {
-    "model_name_or_path": "/home/zhangmin/.kaggle/chatglm2-6b/",
-    "template": "chatglm2",
-    "dataset_dir": "/home/zhangmin/toby/IBA_Project_24spr/data",
-    "dataset": "InsuranceCorpus",
-    # "cutoff_len": 1024,
-    "per_device_train_batch_size": 2,
-    "max_new_tokens": 200,
-    "top_p": 0.8,
-    "temperature": 0.8,
-    "output_dir": "/home/zhangmin/toby/IBA_Project_24spr/saves/exp/insurance_glm_6b",
-    "do_predict": True,
-    "predict_with_generate": True,
-    "split": "validation",
-    "adapter_name_or_path": "/home/zhangmin/toby/IBA_Project_24spr/saves/insurance_glm2_6b",
-    "max_samples": 100,
 }
 
 run_exp(exp1_config)
-run_exp(exp2_config)

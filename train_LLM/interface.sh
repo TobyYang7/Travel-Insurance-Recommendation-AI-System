e4cd6efcbdaea310a05f01bec033fb7d9dede294
TobyYang7 <tobyyang7@foxmail.com>
2024-04-26 00:25:37 +0800

    update

--- train_LLM/interface.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python cli_demo.py \
-    --model_name_or_path /home/zhangmin/toby/IBA_Project_24spr/saves/llama3_v4_insuranceQA \
-    --template llama3 \
-    --finetuning_type freeze \
-    # --adapter_name_or_path ../../saves/LLaMA2-7B/lora/sft \
